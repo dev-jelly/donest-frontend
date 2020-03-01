aeed14ac0106b6b0d5583372fdc00337e4776aeb
jelly <chm073@gmail.com>
2020-03-02 00:43:29 +0900

    Fix Responsive css

--- src/components/item/ItemWrite.tsx
+++ src/components/item/ItemWrite.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+import 'material-icons/iconfont/material-icons.scss';
+
+const JobBlock = styled.div`
+  min-width: 6rem;
+  text-align: center;
+`;
+
+const JobBlockWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: row;
+`;
+
+interface JobProps {
+  icon: string;
+  title: string;
+}
+const Job = ({ icon, title }: PropsWithChildren<JobProps>) => {
+  return (
+    <JobBlock>
+      <p>
+        <span className={'material-icons'}>{icon}</span>
+      </p>
+      <p>{title}</p>
+    </JobBlock>
+  );
+};
 
 const ItemWrite = () => {
   return (
-    <div>
-      This is Item Write
-    </div>
+    <JobBlockWrapper>
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+      <Job title={'공부'} icon={'face'} />
+    </JobBlockWrapper>
   );
 };
 
